c03cd2f350f847ca49b58051bf89f215d31f6c17
ppwagner <pedro.wagner00@edu.pucrs.br>
2022-05-17 16:02:47 -0300

    Adding boss shooting mechanic

--- Bosses/Boss.gd
+++ Bosses/Boss.gd
@@ -4,21 +4,58 @@ signal boss_shooted(bullet, position, direction)
 
 var velocity = Vector2()
 var dir_tiro = Vector2()
+
 var rotation_dir = 0
+onready var rng = RandomNumberGenerator.new().randomize()
+
+enum State {
+	ROTATION_SHOOT,
+	RAIN_SHOOT
+}
 
 export var gravity = 2500
 export (int) var speed = 200
 export (float) var rotation_speed = 1.5
+
 onready var saida_do_tiro := $SaidaTiro
 
+onready var saida_rain1 := $Rain_shoot1
+onready var saida_rain2 := $Rain_shoot2
+onready var saida_rain3 := $Rain_shoot3
+onready var saida_rain4 := $Rain_shoot4
+
+onready var rain_list = [saida_rain1, saida_rain2, saida_rain3, saida_rain4]
+
 export (PackedScene) var Bullet
+export (PackedScene) var Rain_Bullet
 
 func shoot():
 	var bullet_instance = Bullet.instance()
-	var direcao_mouse = saida_do_tiro.global_position.direction_to(dir_tiro).normalized()
+	var direcao_mouse = saida_do_tiro.position.direction_to(dir_tiro).normalized()
 	emit_signal("boss_shooted", bullet_instance, saida_do_tiro.global_position, direcao_mouse)
 	dir_tiro = Vector2(speed, 0).rotated(rotation_dir)
 
+
+func rain_shoot():
+	for tiro in rain_list:
+		var rain_bullet_instance = Rain_Bullet.instance()
+		emit_signal("boss_shooted", rain_bullet_instance, tiro.global_position, Vector2.DOWN)
+
+
+func _on_Timer_timeout():
+	var num = randi() % State.size()
+	if num == 0: #ROTATION_SHOOT
+		for i in range(20):
+			shoot()
+			rotation_dir += 5
+
+	elif num == 1: #RAIN_SHOOT
+		rain_shoot()
+
+
+func _ready():
+	var timer = get_node("Timer")
+	timer.connect("timeout", self, "_on_Timer_timeout")
 
 
 func _physics_process(delta):
@@ -26,6 +63,3 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2.UP)
 	rotation_dir += 0.1
 
-	# (rotation_dir)
-	
-	shoot()
--- Scenes/GameOver.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-
-
-func _on_Button_pressed() -> void:
-	# Trocar o nome da cena principal!
-	get_tree().change_scene("res://Scenes/TesteSprite.tscn")
